1fdec2226d1feafd874441c9fc30f9285b7d1e24
dastyk <dastykill@gmail.com>
2017-04-08 13:55:34 +0200

    Sending B

--- MatMul2DPart/matmul_mpi.c
+++ MatMul2DPart/matmul_mpi.c
@@ -151,8 +151,12 @@ int main(int argc, char **argv)
 		RecvBlock(b, 0, 0,cx,cy, cy, 0, FROM_MASTER);
 		
 		// Send local A one left
-		dest =  y*py + ((x - 1)%px);
+		dest =  y + ((x - 1)%px);
 		SendBlock(a, 0,0, cx, cy, cx, dest, FROM_WORKER);
+		
+		// Send local B one up
+		dest = ((y+1)%py) + x;
+		SendBlock(b, 0,0, cx, cy, cx, dest, FROM_WORKER);
 		
 		
 		free(a);
